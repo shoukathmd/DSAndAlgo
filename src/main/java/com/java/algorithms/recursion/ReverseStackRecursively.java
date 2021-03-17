955a3bc1ab0a2a63866ecffdc83f8779b058b2f3
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2021-03-17 12:13:19 +0530

    Reverse stack recursive Reverse Stack Recursive

---
+++ src/main/java/com/java/algorithms/recursion/ReverseStackRecursively.java
@@ -0,0 +1,13 @@
+package com.java.algorithms.recursion;
+
+import java.util.Stack;
+
+public class ReverseStackRecursively {
+
+
+    public static void reverseRecursively(Stack<Integer> stack) {
+
+    }
+
+
+}
